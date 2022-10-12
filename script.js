d8ce8641dac452ea8486830678433496c7a6cbc2
Imfae <lordoftherin@outlook.com>
2022-10-12 20:58:12 +0800

    Add keyboard support and backspace and cancel buttons

--- script.js
+++ script.js
@@ -37,53 +37,114 @@ let a = [];
 let operator = "";
 let isEqual = false;
 let result = 0;
+let content = "";
+let decimal = document.querySelector("#decimal");
+let decimalPressed = false;
+
 const displayValue = function () {
     
+    //If a button with className "digit" is pressed
     if (this.className === "digit"){
         if (isEqual){
             display.textContent = "";
             isEqual = false;
-        }
-        
-        display.textContent += this.textContent;
-        
-    }
-    if (this.className === "operator"){
-        a.push(Number(display.textContent));
-        if(a.length > 1){
             
-            result = operate(operator, a[0], a[1]);
-            display.textContent = result;
-            a = [];
-            a.push(result);
-            isEqual = true;
-            operator = this.textContent;
-            console.log(a.length);
+        }
+        display.textContent += this.textContent;
+        if (this.textContent === "."){
+            decimal.setAttribute('style', 'pointer-events: none');
+            decimalPressed = true;
+        }
+    }
+
+    //If a button with className "operator" is pressed
+    if (this.className === "operator"){
+        decimalPressed = false;
+        a.push(Number(display.textContent));
+        if (a[1] === 0 && operator === "/"){
+            display.textContent = "";
+            display.textContent = "You should have known better...";
         }
         else{
-            isEqual = false;
-            display.textContent = "";
-            operator = this.textContent;
+            if(a.length > 1){
+                result = operate(operator, a[0], a[1]);
+                display.textContent = result;
+                a = [];
+                a.push(result);
+                isEqual = true;
+                operator = this.textContent;
+                
+            }
+            else{
+                isEqual = false;
+                display.textContent = "";
+                operator = this.textContent;
+            }
         }
         
         
         console.log(`${a[0]} ${a[1]} ${a[2]}`);
     }
+
+    //If a button with className "equal" is pressed
     if (this.className == "equal"){
-        if(operator){
-            a.push(Number(display.textContent));
+        decimalPressed = false;
+        a.push(Number(display.textContent));
+        console.log(`Equal ${a[0]} ${a[1]} ${a[2]}`);
+        if (a[1] === 0 && operator === "/"){
+            display.textContent = "";
+            display.textContent = "You should have known better...";
+        }
+        else{
             result = operate(operator, a[0], a[1]);
             display.textContent = `${result}`;
             a = [];
-            isEqual = true;
-            
+            isEqual = true;    
         }
-        console.log(`${a[0]} ${a[1]} ${a[2]}`);
+        
     }
-    
+
+    //If the cancel button is pressed
+    if (this.textContent === "C"){
+        decimalPressed = false;
+        display.textContent = "";
+        a = [];
+    }
+
+    //If the backspace button is pressed
+    if (this.textContent === "Back"){
+        content = display.textContent;
+        content = content.substring(0, content.length - 1);
+        display.textContent = content;
+    }
+
+    if (decimalPressed === false){
+        decimal.removeAttribute("style", "pointer-events: none");
+    }
 }
 
 const buttons = document.querySelectorAll("button");
+const equal = document.querySelector(".equal");
+const cancel = document.querySelector("#cancel");
+const backspace = document.querySelector("#backspace");
+equal.addEventListener("click", displayValue);
+document.addEventListener('keydown', (event) => {
+	buttons.forEach((button)=>{
+        if (event.key === button.textContent){
+            console.log('button '+ button.className);
+            button.click();
+        }
+    })
+    if (event.key === "Enter"){
+        equal.click();
+    }
+    if (event.key === "Backspace"){
+        backspace.click();
+    }
+    if (event.key === "Escape"){
+        cancel.click();
+    }
+});
 buttons.forEach((button)=>{
     button.addEventListener("click", displayValue);
 });
